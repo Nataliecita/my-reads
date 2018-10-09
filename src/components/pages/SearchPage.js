bde7c28cc06d92144b352bb7630ee2ab5f384803
Natalie Ruiz <natnatruiz@gmail.com>
2018-10-09 16:39:29 -0500

    Query displays books

--- src/components/pages/SearchPage.js
+++ src/components/pages/SearchPage.js
@@ -1,8 +1,74 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
+
+import * as BooksAPI from '../../BooksAPI'
+import Book from '../Book'
+
 
 
 class SearchPage extends React.Component {
+  
+  constructor(props){
+    super(props)
+    this.state = {
+      books: [],
+      results: [],
+      query: ""
+    }
+  }
+
+  componentDidMount(){
+    BooksAPI.getAll()
+      .then(response =>{
+        this.setState({ books: response}) 
+      })
+  }
+
+  updateQuery = (query )=> {
+    // this.setState({ query: query.trim() })
+    this.setState({ query: query }, this.submitQuery)
+  }
+
+  submitQuery() {
+    if(this.state.query === "" || this.state.query === "undefinded"){
+      return this.setState({ results: [] })
+    }
+    BooksAPI.search(this.state.query.trim()).then( response =>{
+      if(response.error){
+        return this.setState({ results: [] })
+      } else {
+        return this.setState({ results: response})
+      }
+    })
+  }
+
+
+  // state = {
+  //   query: ''
+  // }
+
+
+  // if (query) {
+  //     const match = new RegExp(escapeRegExp(query), 'i')
+  //     // showingBooks = books.filter((book) => match.test(book.title))
+  //   } 
+
+    // showingBooks.sort(sortBy('title'))
+
+
+  updateBookShelf = (book, shelf) => {
+      BooksAPI.update(book, shelf)
+          .then(() => {
+            book.shelf = shelf
+            this.setState(state =>({
+              books: state.books.filter(b => b.id !== book.id).concat([book])
+            }))
+          })
+  }
+
+
 	render(){
 		return (
 			          <div className="search-books">
@@ -10,20 +76,17 @@ class SearchPage extends React.Component {
 
             	<Link className="close-search "to="/">Close</Link>
               <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
+                <input type="text" placeholder="Search by title or author" value={this.state.query} onChange={(event) => this.updateQuery(event.target.value)}/>
 
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+              <ol className="books-grid">
+              {
+                this.state.results.map((item, key) =>  <Book updateBookShelf= {this.updateBookShelf} book={item} key={key} />)
+              }
+              </ol>
             </div>
           </div>
 
